d59f37f9ca49655a4ad91e820c21f3d1acabd647
Harth Majeed <harthmajeed@hotmail.com>
2025-09-06 13:43:53 -0400

    logging and debugging

--- main2.py
+++ main2.py
@@ -8,16 +8,21 @@ logger = logging.getLogger(__name__)
 
 @hydra.main(config_path="configs_dir", config_name="config")
 def main(config: DictConfig) -> None:
-    print(OmegaConf.to_yaml(config))
-    print("CURRENT WORKING DIRECTORY: ", os.getcwd())
-    print("ORIGINAL WORKING DIRECTORY: ", get_original_cwd())
-    print("TO ABSOLUTE PATH('some_file.txt'): ", to_absolute_path("some_file.txt"))
-    logger.info("some info message")
-    logger.debug("some debug message")
+    pass
+    # print(OmegaConf.to_yaml(config))
+    # print("CURRENT WORKING DIRECTORY: ", os.getcwd())
+    # print("ORIGINAL WORKING DIRECTORY: ", get_original_cwd())
+    # print("TO ABSOLUTE PATH('some_file.txt'): ", to_absolute_path("some_file.txt"))
+    # logger.info("some info message")
+    # logger.debug("some debug message")
 
 if __name__ == "__main__":
     main()
 
 #python .\main2.py -m tests=resnet18,resnet50 loss_func=arcface,cosface,softmax
 #python .\main2.py -m tests='glob(*)' loss_func='glob(*, exclude=soft*)'
-
+#python .\main2.py --cfg job
+#python .\main2.py --cfg hydra
+#python .\main2.py --cfg all
+#python .\main2.py --cfg job --package tests
+#python .\main2.py --cfg job --package loss_func
