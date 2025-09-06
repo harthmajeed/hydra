ea8bd47cc00064b1c7baec822ed09d7dc138249c
Harth Majeed <harthmajeed@hotmail.com>
2025-09-06 13:34:29 -0400

    logging

--- main2.py
+++ main2.py
@@ -1,9 +1,19 @@
 from omegaconf import DictConfig, OmegaConf
 import hydra
+from hydra.utils import get_original_cwd, to_absolute_path
+import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 @hydra.main(config_path="configs_dir", config_name="config")
 def main(config: DictConfig) -> None:
     print(OmegaConf.to_yaml(config))
+    print("CURRENT WORKING DIRECTORY: ", os.getcwd())
+    print("ORIGINAL WORKING DIRECTORY: ", get_original_cwd())
+    print("TO ABSOLUTE PATH('some_file.txt'): ", to_absolute_path("some_file.txt"))
+    logger.info("some info message")
+    logger.debug("some debug message")
 
 if __name__ == "__main__":
     main()
